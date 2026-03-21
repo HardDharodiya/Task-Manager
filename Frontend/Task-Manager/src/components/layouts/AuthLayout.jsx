727e791d5c9cdce7f858846206954819c42608f8
HardDharodiya <dharpdiyaneev07@gmail.com>
2026-03-21 18:01:41 +0530

    .

--- Frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
+++ Frontend/Task-Manager/src/components/layouts/AuthLayout.jsx
@@ -5,7 +5,7 @@ const AuthLayout = ({ children }) => {
     return (
         <div className='flex'>
             <div className='w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12'>
-                <h2 className='text-lg font-semibold text-black'>Task Manager</h2>
+                <h2 className='text-lg font-semibold text-black'>Task Room</h2>
                 {children}
             </div>
 
